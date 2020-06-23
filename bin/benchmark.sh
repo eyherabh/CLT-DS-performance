9077816ee80526669c564d4fc185db1d9bdb35da
Hugo Gabriel Eyherabide <hugo.eyherabide@bcplatforms.com>
2020-06-23 17:45:17 +0300

    Updated scripts

--- bin/benchmark.sh
+++ bin/benchmark.sh
@@ -1,4 +1,21 @@
-# Runs test; computes median and quantiles
+#!/bin/bash 
+#
+# Copyright (c) 2020 Ph.D. Hugo Gabriel Eyherabide
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# \brief Runs test; computes median and quantiles
 
 POSIXLY_CORRECT=1
 IFS=$' \t\n'
@@ -20,10 +37,16 @@ where <script path> is the path to the test script.
 
 [ $# -eq 1 ] && [ -f "$1" ] || print_usage
 
-tmpfile=$(mktemp benchmark_"${1//\//.}"_XXXXXX) || fail "Cannot create temporary file for benchmarking"
+tmpfile=$(mktemp benchmark/benchmark_"${1//\//.}"_XXXXXX) || fail "Cannot create temporary file for benchmarking"
 
 for i in {1..9}; do
 	\time -f '%e' -o "$tmpfile" -a bash "$1" &>/dev/null || fail "Fail while running test script"
 done
 
-bin/get_percentiles.awk 25 50 75 < "$tmpfile"
+title="Percentiles"
+value="Duration"
+while read -r percentile duration; do
+	title+="\t$percentile"
+	value+="\t$duration"
+done < <(bin/get_percentiles.awk 25 50 75 < "$tmpfile" | tail -n +2)
+printf "$title\n$value\n"
--- tests/test.2.4.sh
+++ tests/test.2.4.sh
@@ -1,7 +1,1 @@
-set -o pipefail
-for i in {0..99}; do printf -- "data/cgdata.00.pgn\0"; done \
-	| xargs -0 -n4 -P4 mawk '
-	  	      		/^\[Result / && NF==2 { res[$2]++ } 
-				END { print res["\"1-0\"]"], res["\"0-1\"]"], res["\"1/2-1/2\"]"], res["\"*\"]"] }' \
-	| mawk '{white += $1; black += $2; draw += $3; unknown += $4} 
-	        END { print white+black+draw+unknown, white, black, draw, unknown }'
+tests/test_gen.sh 100 tests/map_step_accurate_unkown.awk tests/reduce_step_accurate.awk
